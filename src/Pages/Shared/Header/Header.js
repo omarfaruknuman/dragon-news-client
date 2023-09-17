2bbb01a2e327926adb6cfb60f0fd5db9e57e8a78
Md Omar Faruk Numan <omarfaruk1271@gmail.com>
2023-09-17 11:16:33 +0600

    Updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import { FaUser} from 'react-icons/fa';
+import { Button, Image } from 'react-bootstrap';
 
 const Header = () => {
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () =>{
+        logOut()
+        .then( ()=> {})
+        .catch(error => console.error(error))
+    }
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary mb-4">
             <Container>
@@ -17,9 +27,32 @@ const Header = () => {
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link >
+                            {
+                                user?.uid ? 
+                                <>
+                                    <span>{user?.displayName}</span>
+                                    <Button onClick={handleLogOut} variant="light">Log Out</Button>
+                                </>
+                                :
+                                <>
+                                    <Link to='/login'>Login</Link>
+                                    <Link to='/register'>Register</Link>
+                                </>
+                            }
+                            
+                            </Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                          {
+                            user?.photoURL ?
+                            <Image 
+                                style={{height: '30px'}}
+                                roundedCircle
+                                src={user?.photoURL}
+                            ></Image>
+                            :
+                            <FaUser></FaUser>
+                          }
                         </Nav.Link>
                     </Nav>
                     <div className='d-lg-none'>
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,50 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState();
+    
+    const providerLogin = (provider) =>{
+        return signInWithPopup( auth, provider);
+    }
+
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () =>{
+        return signOut(auth);
+
+    }
+
+    useEffect( () =>{
+        const unSubscribe =  onAuthStateChanged(auth, (currentUser) =>{
+            console.log('inside auth state change', currentUser);
+            setUser(currentUser)
+        });
+        return () =>{
+            unSubscribe();
+        }
+    },[])
+
+
+    const authInfo = { user ,providerLogin ,logOut ,createUser , signIn}
+
+    return (
+        <AuthContext.Provider value={ authInfo }>
+                {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
